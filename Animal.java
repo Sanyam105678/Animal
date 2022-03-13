61102bba53a3d584145074c86975e44cac2545e0
Sanyam105678 <96380392+Sanyam105678@users.noreply.github.com>
2022-03-13 20:58:53 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public abstract class Animal {
+    public abstract void makeNoise();
+
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+
+    public void walk(){
+        System.out.println("Animal is walking");
+    }
+
+
+}
